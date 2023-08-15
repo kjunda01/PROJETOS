76650b664fa42805c1bb772e3f6ab2246a6f11de
kjunda01 <sinvaljuniorlms@gmail.com>
2023-08-14 21:52:19 -0300

    initial commit

---
+++ Python_3_do_Basico_Ao_Avancado/36. Listas em Python/Aula 21.py
@@ -0,0 +1,19 @@
+"""
+Listas em Python
+Fatiamento
+append, insert, pop, del, clear, extend, +
+min, max
+range
+"""
+
+# Valores positivos
+#         0    1    2    3    4
+lista = ['A', 'B', 'C', 'D', 'E']
+#        -5   -4   -3   -2   -1
+#Valores negativos
+
+string = 'BCDE'
+
+print(string[1])
+print(lista[0])
+
