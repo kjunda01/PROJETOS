ac1500e6109de23ecb143e5751119c1fabd5126e
kjunda01 <kjunda01@sinvaljuniorlms@gmail.com>
2023-08-22 22:29:10 -0300

    commit

--- Testes/Arquivos/Prefeitura vai implantar testes rápidos de saúde/Renomear com python.py
+++ Testes/Arquivos/Prefeitura vai implantar testes rápidos de saúde/Renomear com python.py
@@ -5,81 +5,46 @@ import glob
 
 # VARIÁVEIS INICIAIS
 main_folder_path = os.getcwd()
-main_folder_only = os.getcwd().split('/')[-1]
-
 
 # ********** COMEÇO DO PROGRAMA **********
 
 # EXTRAIR OS ARQUIVOS ZIP
-def zip_extract():
-    print('Extraindo arquivos .zip..')
-    zip_list = glob.glob('*.zip')
-    for zips in zip_list:
-        shutil.unpack_archive(zips)
-        print(f'Arquivo "{zips}" extraído com sucesso.')
+print('Extraindo arquivos .zip..')
+zip_list = glob.glob('*.zip')
+for zips in zip_list:
+    shutil.unpack_archive(zips)
+
 
 
 # COPIAR O É NOTICIA PARA O WORKING DIRECTORY
-def copy_cdr():
-    e_noticia_folder = '/home/kjunda01/Downloads/É NOTÍCIA - MODELO.cdr'
-    print(f'Copiando arquivo "{e_noticia_folder.split("/")[-1]}" para a pasta "{main_folder_only}".')
-    shutil.copy2(e_noticia_folder, main_folder_path)
+e_noticia_folder = 'C:/Users/Kjunda01/OneDrive - 2s1xlx/SEMED 2020/COMUNICAÇÃO PREFEITURA/Publicações/Modelo para publicação/Modelo/É NOTÍCIA - MODELO.cdr'
+shutil.copy2(e_noticia_folder, main_folder_path)
 
 # FUNÇÕES DE RENOMEAR ARQUIVOS
-def rename_cdr():
-    dir_list = glob.glob('*')
-    counter_cdr = 1
-    for files in dir_list:
-        file_extention = f'.{files.split(".")[-1]}'
-        old_file_name = main_folder_path + '/' + files
 
-        # RENOMEAR CDR
-        if file_extention.endswith('.cdr'):
-            new_name_only_cdr = f'É NOTÍCIA - {main_folder_only[:30]}({counter_cdr}){file_extention}'
-            new_path_cdr = os.path.join(main_folder_path, new_name_only_cdr)
-            if not os.path.exists(new_path_cdr):
-                os.rename(old_file_name, new_path_cdr)
-                print(f'"{files}" ----> "{new_name_only_cdr}".')
-                counter_cdr += 1
+for diretory, subfolder, folder in os.walk(main_folder_path):
+    for files in folder:
+        counter_cdr = 1
+        counter_jpeg = 1
+        # old_name = os.path.join(os.path.realpath(diretory), files)
+        # files = os.path.basename(old_name)
+        main_folder_35 = os.path.basename(main_folder_path)[:35]
+        
+        if files.endswith('.cdr'):
 
-
-def rename_jpeg():
-    dir_list = glob.glob('*')
-    counter_jpeg = 1
-    for files in dir_list:
-        file_extention = f'.{files.split(".")[-1]}'
-        old_file_name = main_folder_path + '/' + files
-
-        # RENOMEAR JPEG
-        if file_extention.endswith('.jpeg'):
-            new_name_only_jpeg = f'É NOTÍCIA - {main_folder_only[:30]} - SITE({counter_jpeg}){file_extention}'
-            new_path_jpeg = os.path.join(main_folder_path, new_name_only_jpeg)
-            if not os.path.exists(new_path_jpeg):
-                os.rename(old_file_name, new_path_jpeg)
-                print(f'"{files}" ----> "{new_name_only_jpeg}".')
-                counter_jpeg += 1
+            new = f"'É NOTÍCIA - {main_folder_35}({str(counter_cdr)}).cdr"
+            counter_cdr += 1
+            os.rename(files, new)
+             
+        elif files.endswith('.jpeg'):
+            new = f"É NOTÍCIA - {main_folder_35}({str(counter_jpeg)}).jpeg"
+            counter_jpeg += 1
+            os.rename(files, new) 
+            
+                
 
 
-def rename_zip():
-    dir_list = glob.glob('*')
-    counter_zip = 1
-    for files in dir_list:
-        file_extention = f'.{files.split(".")[-1]}'
-        old_file_name = main_folder_path + '/' + files
 
-        # RENOMEAR ZIP
-        if file_extention.endswith('.zip'):
-            new_name_only_zip = f'{main_folder_only[:30]} - ZIP({counter_zip}){file_extention}'
-            new_path_zip = os.path.join(main_folder_path, new_name_only_zip)
-            os.rename(old_file_name, new_path_zip)
-            print(f'"{files}" ----> "{new_name_only_zip}".')
-            counter_zip += 1
-
-copy_cdr()
-zip_extract()
-rename_cdr()
-rename_jpeg()
-rename_zip()
 
 # MENSAGEM FINAL
 print('Tarefas concluídas com successo!')
--- teste.py
+++ teste.py
@@ -1,2 +1,15 @@
-import glob
+import os 
 
+main_folder_path = os.getcwd()
+
+dest = main_folder_path.rfind(f'{os.path.basename(main_folder_path)}')
+
+barra = main_folder_path.rfind(f'{os.path.basename(main_folder_path)}')-1
+
+main = main_folder_path[barra:]
+print(os.walk(main_folder_path))
+
+
+print(dest)
+print(barra)
+print(main[:30])
