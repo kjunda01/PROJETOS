e22d7e47db8aed337f3bef7e68feb9334efa8406
Kjunda01 <sinvaljuniorlms@gmail.com>
2023-08-24 16:02:05 -0300

    commit

--- 3. Python Intermediário (Programação Procedural)/1. Funções (def) em Python - Parte 1/Aula 1.py
+++ 3. Python Intermediário (Programação Procedural)/1. Funções (def) em Python - Parte 1/Aula 1.py
@@ -1,4 +1,20 @@
-""""
-Funções - Def em Python (Parte 1)
 """
 
+Funções - def em Python (Parte 1)
+
+"""
+
+# def saudacao(msg='Olá', nome='usuário'):
+#     print(msg, nome)
+
+# saudacao('Opa', 'Moab')
+# saudacao('','')
+
+from getpass import getuser
+def saudacao(msg='Olá', nome=getuser()):
+    nome = nome.replace('E', '3')
+    msg = msg.replace('á', '@')
+    return f'{msg}, {nome}'
+
+variavel = saudacao()
+print(variavel)
